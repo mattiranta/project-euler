b0c48e36eaabd929f89bc326063f3447c0ec5ef8
Matti Ranta (work) <matti@ranta.co>
2014-02-07 16:08:49 +0200

    18 continued

--- Python/18/18.py
+++ Python/18/18.py
@@ -4,21 +4,25 @@ import sys
 
 input = \
 """
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
+                 75
+                95 64
+               17 47 82
+             18 35 87 10
+            20 04 82 47 65
+           19 01 23 75 03 34
+         88 02 77 73 07 63 67
+        99 65 04 28 06 16 70 92
+       41 41 26 56 83 40 80 70 33
+      41 48 72 33 47 32 37 16 94 29
+     53 71 44 65 25 43 91 52 97 51 14
+    70 11 33 28 77 73 17 78 39 68 17 57
+   91 71 52 38 17 14 91 43 58 50 27 29 48
+  63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
+
+test = """62 98 98 27 23 70 98 98 93 93 53 60 60 23
+            98 98 98 27 70 98 98 98 93 93 60 60 60
+	      63 66 04 68 89 53 67 30 73 16 69 87 40 31"""
 
 def read_input(input):
     rows = []
@@ -29,15 +33,28 @@ def read_input(input):
     return rows
 
 def max_sum(rows):
-	sum_list = [0] * len(rows[-1])
+	sum_list = [0] * len(rows)
 	print(len(sum_list))
 	print('---------')
-	for i in rows[::-1]:
-		for j in i[1:]:
+	#print('{}:\t{}'.format(0, sum_list))
+	offset = 0
+	for index, i in enumerate(rows[::-1]):
+		for index, val in enumerate(i[:-1]):
+			print('{} - ({}, {})'.format(index, i[index], i[index + 1]))
+			sum_list[index + offset] += max(i[index], i[index + 1])
+			print('sum at {}: {}'.format(index, sum_list[index -1 ]) )
 
+		print(sum_list)
+		if offset > 2:
+			break;
+		print('###############')
+		offset += 1
+		if(len(sum_list) == index):
+			print('{}:\t{}'.format(index, sum_list))
+		else:
+			print('{}:\t{}'.format(index, sum_list[0:-(len(sum_list)-index)]))
 
-	sum = 0
-	return sum
+	return max(sum_list)
 
 
 # Main:
